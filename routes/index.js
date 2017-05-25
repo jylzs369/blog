7699efe5f13f80762d3f945c1aa475d26ab269e5
Ushio <jylzs369@163.com>
2017-05-25 22:49:44 +0800

    modify views

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,25 @@
+var crypto = require('crypto'),
+    User = require('../models/user.js');
+
 module.exports = function(app) {
     /* GET home page. */
     app.get('/', function(req, res, next) {
-            res.render('index', { title: '主页' });
+            res.render('index', { title: '主页', home: true });
         })
-        .get('/reg', function(req, res, next) {
-            res.render('reg', { title: '注册' });
+        .get('/register', function(req, res, next) {
+            res.render('register', { title: '注册', register: true });
         })
-        .post('/reg', function(req, res, next) {
-
+        .post('/register', function(req, res, next) {
+            var name = req.body.name;
         })
         .get('/login', function(reg, res, next) {
-            res.render('login', { title: '登录' });
+            res.render('login', { title: '登录', login: true });
         })
         .post('/login', function(reg, res, next) {
 
         })
         .get('/post', function(reg, res, next) {
-            res.render('post', { title: '发表' });
+            res.render('post', { title: '发表', currPage: 'post' });
         })
         .post('/post', function(reg, res, next) {
 
