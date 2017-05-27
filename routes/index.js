d4f036a375bef0eb6a7e77e9bc8cf1102639593c
Ushio <jylzs369@163.com>
2017-05-27 17:41:10 +0800

    modify routes

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,31 @@ module.exports = function(app) {
             res.render('register', { title: '注册', register: true });
         })
         .post('/register', function(req, res, next) {
-            var name = req.body.name;
+            var user = {
+                name: req.body.name,
+                password: req.body.password,
+                email: req.body.email
+            }
+            if (!user.name) {
+                res.send('姓名不能为空');
+                return;
+            }
+            if (!user.password) {
+                res.send('密码不能为空');
+                return;
+            }
+            if (!user.email) {
+                res.send('邮箱不能为空');
+                return;
+            }
+            var newUser = new User(user);
+            newUser.save(function (err) {
+                if (err) {
+                    console.log(err)
+                    return;
+                }
+                res.send('success');
+            });
         })
         .get('/login', function(reg, res, next) {
             res.render('login', { title: '登录', login: true });
